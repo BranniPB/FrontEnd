bd021874e3c798661bb5fa8cb0aef1ff450b0800
unknown <branni.bazurto@uisek.edu.ec>
2024-01-30 22:50:19 -0500

    Login FrontEnd

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+import Profile from './components/profile';
+import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+
+  const {isAuthenticated, isLoading} = useAuth0()
+
+  if (isLoading) return  <h1>Loading....</h1>
+  return (
+    <div className="App">
+      <h1>Application</h1>
+    {isAuthenticated ? <LogoutButton/>:<LoginButton/>}
+    <Profile/>
+    </div>
+  );
+}
+
+export default App;
